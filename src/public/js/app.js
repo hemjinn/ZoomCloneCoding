aa216a4623bb42a5bc8e4ddbf521612595d9b228
ssepssep <guswls7816@naver.com>
2023-07-14 14:58:45 +0900

    Update setting text type ver0.2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,50 @@
-alert("hi");
+// 브라우저에서 backend와 connection을 열어주는 socket을 선언한다.
+const socket = new WebSocket(`ws://${window.location.host}`);
+// html로부터 Element를 불러온다.
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+socket.addEventListener("open", () => {
+    // 브라우저에서 서버와 연결되었음을 출력
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+    // 브라우저에서 소켓으로 부터 받은 데이터를 메시지로 출력
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.appendChild(li);
+});
+
+socket.addEventListener("close", () => {
+    // 브라우저에서 서버와 연결이 끊겼음을 출력
+    console.log("Disconnected from server ❌")
+});
+
+function makeMassage(type, payload) {
+    // JSON object를 string타입으로 변환하는 함수
+    // 이게 Serialization이 아닌가 싶음.
+    // 이 서버는 JS로 만들었는데, 누군가는 GO를 이요해 서버에 접속하려 할 수 있기때문에 string으로 값을 전달해야한다.
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+function handleSubmit(event) {
+    // 메시시 submit 이벤트에 작동하는 함수
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMassage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSave(event) {
+    // 닉네임 submit 이벤트에 작동하는 함수
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    // 백엔드는 javascript object를 인식하지 못하기 때문
+    socket.send(makeMassage("nickname", input.value));
+} 
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSave);
--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ import http from "http";
 import WebSocket, { WebSocketServer } from 'ws';
 import express from "express";
 
+const date = new Date();
+function realTime() {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `[${hours}:${minutes}:${seconds}]`;
+}
+
+
 // 1. 앱을 만든다.
 const app = express();
 
@@ -18,6 +27,7 @@ app.use("/public", express.static(__dirname + "/public"));
 // 4. 라우터들을 만든다. 홈을 렌더링 함.
 // "/" 주소에서 home.pug를 render 해주는 route handler를 만든다.
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (_, res) => res.render("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
@@ -28,12 +38,48 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-function handleConnection(socket) {
-    // socket은 나(서버)와 브라우저 사이의 연결이다.
-    console.log(socket);
-}
+// 서버에 접속한 이용자 수를 체크하기 위해 fake DB를 하나 만든다.
+const sockets = [];
 
 // on method는 socket으로부터 backend에 연결된 사람의 정보를 제공해준다.
-wss.on("connection", handleConnection);
+// socket은 나(서버)와 브라우저 사이의 연결이다.
+wss.on("connection", (socket) => {
+    // edge, chrome 등 연결된 브라우저의 socket을 sockets array에 넣어줌으로써
+    // sockets에 있는 모든 곳에 전달해 줄 수 있게된다.
+    sockets.push(socket);
+    // 연결 전 닉네임이 주어지지 않은 사용자들의 초기 닉네임값 설정
+    socket["nickname"] = "Unknown";
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => {
+        console.log("Disconnection from the Browser ❌");
+    });
+    socket.on("message", (msg) => {
+        // 브라우저에서 서버로 보낸 메시지를 받아서 브라우저로 출력시킨다.
+        const parsedMsg = JSON.parse(msg);
+        switch (parsedMsg.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${realTime()} ${socket.nickname}: ${parsedMsg.payload}`));
+                break;
+            case "nickname":
+                // socket이 누구야? -> nickname을 socket안에 넣어줘야함.
+                // socket은 기본적으로 객체기 때문에 새로운 item을 추가할 수 있다.
+                socket["nickname"] = parsedMsg.payload; 
+                break;
+        }
+    });
+});
 
 server.listen(3000, handleListen);
+
+// text에 서로의 닉네임을 붙혀서 누가 어떤 대화를 하는지 표시하고 싶어서 닉네임을 back-end단에 저장해주어야하는데
+// 같은 text 타입이라 닉네임과 메시지가 구별이 안된다.
+// 사용하는 text의 타입을 '메시지'용도와 '닉네임'용도로 나눈다.
+{
+    type: "message";
+    payload: "hello everyone!";
+}
+
+{
+    type:"nickname";
+    payload:"hemjin";
+}
